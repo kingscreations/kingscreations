031232fb2594b085a42827406f894e907ef08728
Jason King <jking109@cnm.edu>
2015-01-14 09:35:03 -0700

    First entry

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+		<head>
+				<title>Personal Web Site</title>
+		</head>
+		<body>
+			<p>This is my personal web site. It is going to be awesome in 8 weeks!</p>
+		</body>
+</html>
