ea4bb0ebeea9975215cddfa4dbc481d7c17863a3
Jason King <jking109@cnm.edu>
2015-03-02 18:34:57 -0700

    deleted old format/created single page layout

--- index.php
+++ index.php
@@ -1,29 +1,131 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		<!-- Custom CSS -->
-		<link type="text/css" href="../css/style.css" rel="stylesheet"/>
-		<!-- stellar.js plugin -->
-		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<title>King's Creations</title>
-	</head>
-		<body>
-			<div class="class=container">
-				<div class="row">
-					<div class="jumbotron" id="welcome">
-						<h1>Jason King</h1>
-					</div> <!-- end jumbotron-->
-					<div class="row" id="phrase">
-						<!--html-->
-						<h2>Owner, Creator, and Founder of King's Creations</br>
-						   My friends just call me <em>Grand Poobah</em></h2>
-						<!--	href to next page-->
 
-					</div> <!--end phrase-->
-			</div><!-- end container-->
-		</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>King's Creations</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/prefixfree.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script src="js/myscript.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body>
+
+		<header>
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div><!-- navbar-header -->
+					<div class="collapse navbar-collapse" id="collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="#featured">Home</a></li>
+							<li><a href="#introduction">Introduction</a></li>
+							<li><a href="#history">history</a></li>
+							<li><a href="#qAndA">Q and A</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+					</div><!-- collapse navbar-collapse -->
+				</div><!-- container -->
+			</nav>
+			<div class="jumbotron">
+				<h1>Kings Creation's </br>
+					Web Development</h1>
+			</div><!-- jumbotron-->
+
+		</header>
+
+		<div class="main">
+			<div class="page" id="introduction">
+				<div class="content container">
+					<h2>Introduction</h2>
+					<div class="row">
+						<p class="col-md-5 col-md-offset-1">My Name is Jason King. I know a great lotta stuff... everything except web design. Im pretty sure Im clueless at that. I can pretend though. Im really good at that. I once convinced a nun I was an alien from another planet..</p>
+						<p class="col-md-5">Im a lumberjack and Im OK, I sleep all night, work all day. I chop down trees, I fish and hunt... I like to press wild flowers. I put on womens clothing, and hang around in bars...just like my dear maa maa./p>
+					</div><!-- row -->
+				</div><!-- content container -->
+			</div><!-- introduction page -->
+
+			<div class="page" id="history">
+				<div class="container-fluid">
+					<h2 id="workHistory">History</h2>
+					<div class="row">
+						<div class="workPicture col-lg-4">
+							<div class="row">
+								<div class="photo col-xs-6 col-xs-offset-3 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 col-lg-4 col-lg-offset-0">
+									<img class="img-circle" src="images/jetFighter.jpg" alt="Photo of a Jet Fighter">
+								</div><!-- photo -->
+								<div class="info col-xs-8 col-xs-offset-2 col-sm-7 col-sm-offset-0 col-md-6 col-lg-8">
+									<h3>US Air Force</h3>
+									<p>I was in the Air Force. A long long time ago. In fact, it was before they had invented air planes. We used to run around the air strip pretending we were flying. It was great fun.</p>
+								</div><!-- info -->
+							</div> <!-- inner row -->
+						</div> <!-- workPicture -->
+					</div><!-- outer row -->
+					<div class="row">
+						<div class="workPicture col-lg-4">
+							<div class="row">
+								<div class="photo col-xs-6 col-xs-offset-3 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 col-lg-4 col-lg-offset-0">
+									<img class="img-circle" src="images/warehouse.jpg" alt="Photo of a warehouse">
+								</div><!-- photo -->
+								<div class="info col-xs-8 col-xs-offset-2 col-sm-7 col-sm-offset-0 col-md-6 col-lg-8">
+									<h3>Symphony Seed Paper</h3>
+									<p>I made handmade paper with hooligans. To show we were really tough we put seeds inside the paper and sprinkled the outside with glitter. It was a very happy place.</p>
+								</div><!-- info -->
+							</div> <!-- inner row -->
+						</div> <!-- workPicture -->
+						<div class="row">
+							<div class="workPicture col-lg-4">
+								<div class="row">
+									<div class="photo col-xs-6 col-xs-offset-3 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 col-lg-4 col-lg-offset-0">
+										<img class="img-circle" src="images/woodshop.jpg" alt="Photo of a woodshop">
+									</div><!-- photo -->
+									<div class="info col-xs-8 col-xs-offset-2 col-sm-7 col-sm-offset-0 col-md-6 col-lg-8">
+										<h3>King's Creations Fine Furniture and Construction</h3>
+										<p>For a very long time I made furniture and fixed houses. Not sure why they are grouped together. Most carpenters wouldn't know how to start a bandsaw, much less keep all 11 fingers.</p>
+									</div><!-- info -->
+								</div> <!-- inner row -->
+							</div> <!-- workPicture -->
+						</div><!-- outer row -->
+					</div><!-- container -->
+				</div><!-- history page -->
+
+				<div class="page" id="qAndA">
+					<div class="container-fluid">
+						<h2>Q and A</h2>
+						<div class="row">
+							<article class="service col-md-4 col-sm-6 col-xs-12">
+								<img class="icon" src="images/questionMark.png" alt="Icon">
+								<blockquote class="col-sm-12" id="q1">
+									<div class="question">
+										<span class="question">great big question. very deep...philosophical perhaps?</span>
+										<span class="answer">pause for consideration. nod. strike an intellectual pose. Maybe have an old fashioned pipe and a really cool hat. say: blah, blah, blah, pause for emphasis...blah. Repeat as needed.</span>
+									</div>
+								</blockquote>
+
+						</div>
+					</div><!-- container -->
+				</div><!-- Q and A page -->
+			</div><!-- main -->
+
+			<footer>
+				<div class="content container-fluid">
+					<div class="row">
+						<div class="col-sm-6">
+							<p>Contact me at <span class="phone">719-696-1772</span></p>
+							<p> or email me at <span class="email">jason@kingscreations.org</span></p>
+						</div><!-- col-sm-6 -->
+					</div><!-- row -->
+				</div><!-- content container -->
+			</footer>
+	</body>
 </html>
